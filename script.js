d69ad56287730873aa7b81db15082023f60ddb6b
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-09-02 00:02:25 +0800

    Update game start message to include a welcome note.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('start-button').addEventListener('click', function() {
     console.log('Start button clicked.');
-    document.getElementById('game-info').innerText = 'The game is starting! Good luck!';
+    document.getElementById('game-info').innerText = 'Welcome to the game! The game is starting! Good luck!';
     // Game logic could be added here
 });
