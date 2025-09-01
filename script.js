b276e2797bd8458dbe348f46dba202b527f40cba
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-09-02 00:01:03 +0800

    feat: initial commit for script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('start-button').addEventListener('click', function() {
+    document.getElementById('game-info').innerText = 'Das Spiel beginnt! Viel Glück!';
+    // Hier könnte Logik für das Spiel eingefügt werden
+});
