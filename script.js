4d113f7f834a6efeb559776aa259fb2726869875
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-09-02 00:01:50 +0800

    Added a console log for better debugging during game start.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById('start-button').addEventListener('click', function() {
+    console.log('Start button clicked.');
     document.getElementById('game-info').innerText = 'The game is starting! Good luck!';
     // Game logic could be added here
 });
